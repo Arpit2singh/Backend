c25f0b02c8dd7f8cf575802324617efac26152fc
Arpit Singh <arpitno57@gmail.com>
2025-07-10 23:43:35 +0530

    ds

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const DBNAME = "videotube" ;
 
+export const CLOUDINARY_CLOUD_NAME = `dow8ovkmb` ;
+
+export const CLOUDINARY_API_KEY= `253948419817179` ; 
+ 
+export const CLOUDINARY_API_SECRET= `kFRMm5oPe5OGCWiX0BO1PNtSPVk` ; 
