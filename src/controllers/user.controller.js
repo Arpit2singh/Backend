beb217a0931c73b2687be682ee4dfb872585b6d8
Arpit Singh <arpitno57@gmail.com>
2025-07-10 23:49:25 +0530

    change in the controller coverImage check

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,7 +54,9 @@ if (existedUser) {
    const avatarLocalPath = req.files?.avatar[0]?.path;
    let coverImageLocalPath ; 
 
-   if()
+   if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+      coverImageLocalPath = req.files.coverImage[0].path ; 
+   }
   
 
 
